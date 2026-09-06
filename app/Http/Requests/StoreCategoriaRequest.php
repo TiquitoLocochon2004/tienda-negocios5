d3a6969b36e351e8bf5881eadb4b11695875c850
TiquitoLocochon2004 <martinezgodoyagustin@gmail.com>
2026-09-06 20:43:47 -0300

    Entregable 5 Completado

--- app/Http/Requests/StoreCategoriaRequest.php
+++ app/Http/Requests/StoreCategoriaRequest.php
@@ -27,9 +27,7 @@ class StoreCategoriaRequest extends FormRequest
 
     public function rules(): array
     {
-        //$categoriaId = $this->route('categoria') ? $this->route('categoria')->id : null;
-        $categoria = $this->route('categoria');
-        $categoriaId = is_object($categoria) ? $categoria->id : $categoria;
+        $categoriaId = $this->route('categoria') ? $this->route('categoria')->id : null;
 
         return [
             'nombre' => 'required|string|max:50|unique:categorias,nombre,|regex:/^[\pL\s]+$/u|' . $categoriaId,
