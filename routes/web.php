bae1fafc3cbe99c526d1323b48abb2ee016491ce
TiquitoLocochon2004 <martinezgodoyagustin@gmail.com>
2026-09-06 05:02:00 -0300

    Entregable 5 Completado

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,26 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\CarritoItemController;
 
+// Route::view('/', 'welcome');
+
+// Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
+// Route::get('/productos/crear', [ProductoController::class, 'create'])->name('productos.create');
+// Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
+// Route::delete('/productos/{producto}', [ProductoController::class, 'destroy'])->name('productos.destroy');
+// Route::get('/productos/{producto}/editar', [ProductoController::class, 'edit'])->name('productos.edit');
+// Route::put('/productos/{producto}', [ProductoController::class, 'update'])->name('productos.update');
+
+// Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
+// Route::resource('categorias', CategoriaController::class);
+// Route::post('/carrito/agregar', [CarritoItemController::class, 'store'])->name('carrito.agregar')->middleware('auth');
+
 Route::view('/', 'welcome');
 
-Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
-Route::get('/productos/crear', [ProductoController::class, 'create'])->name('productos.create');
-Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
-Route::delete('/productos/{producto}', [ProductoController::class, 'destroy'])->name('productos.destroy');
-Route::get('/productos/{producto}/editar', [ProductoController::class, 'edit'])->name('productos.edit');
-Route::put('/productos/{producto}', [ProductoController::class, 'update'])->name('productos.update');
+// Rutas completas para Productos
+Route::resource('productos', ProductoController::class);
 
-Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
+// Rutas completas para Categorías
 Route::resource('categorias', CategoriaController::class);
+
+// Ruta del carrito
 Route::post('/carrito/agregar', [CarritoItemController::class, 'store'])->name('carrito.agregar')->middleware('auth');
